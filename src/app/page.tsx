ba6c2b37dc431616650de778f52954e2255fb938
monire91 <aglida1370@gmail.com>
2025-02-18 17:22:11 -0600

    updated style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import SelectedArticles from "./_components/selectedArticles";
 
 export default function Home() {
   return (
-    <div className="w-full max-w-full flex justify-center gap-6 *:w-[45%] p-6">
+    <div className="w-full max-w-full flex flex-col md:flex-row justify-center gap-6 *:w-full md:*:w-[45%] *:p-6 ">
       <News />
       <SelectedArticles />
     </div>
